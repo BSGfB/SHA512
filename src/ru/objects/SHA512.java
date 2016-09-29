004122c7ebb2db64fb7d5ec677d7d89bc4d075be
BSGfB <mr-serj@outlook.com>
2016-09-29 22:54:28 +0300

    Without main loop

--- src/ru/objects/SHA512.java
+++ src/ru/objects/SHA512.java
@@ -1,56 +1,213 @@
 package ru.objects;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
 import java.math.BigInteger;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.ArrayList;
 import java.util.Arrays;
 
-public class SHA512 {
-	private static final BigInteger[] H = new BigInteger[] {
-			new BigInteger("6a09e667f3bcc908"),
-			new BigInteger("bb67ae8584caa73b"),
-			new BigInteger("3c6ef372fe94f82b"),
-			new BigInteger("a54ff53a5f1d36f1"),
-			new BigInteger("510e527fade682d1"),
-			new BigInteger("9b05688c2b3e6c1f"),
-			new BigInteger("1f83d9abfb41bd6b"),
-			new BigInteger("5be0cd19137e2179")
-	}; 
-	
-	
-	public void get1024BitBlocks(BigInteger base) {
-		int Size = base.bitLength() / 1024 + 1;
-		BigInteger[] Blocks_1024 = new BigInteger[Size];
-		byte[] array = base.toByteArray();
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+public class SHA512 {	
+	public static byte[][] get128BytesBlocks(byte[] array) {
+		int mod = array.length % 128;
+		int amount = array.length / 128 + (128 - mod > 8 ? 1: 2);
+		byte[][] blocks = new byte[amount][];
 		
-		for(int i = 0; i < Size - 1; i++) {
-			byte[] tempArray = Arrays.copyOfRange(array, i * 128, (i + 1) * 128);
-			Blocks_1024[i] = new BigInteger(tempArray);
+		System.out.println("Mod: " + mod + " amount: " + amount);
+		
+		for(int i = 0; i < array.length / 128; i++) {
+			blocks[i] = Arrays.copyOfRange(array, i * 128, (i + 1) * 128);
 		}
 		
-		if(64 < (base.bitLength() % 1024)) {
-			byte[] tempArray = Arrays.copyOfRange(array, (Size - 1) * 128, array.length - 1);
-			byte[] Array128 = new byte[128];
-			Arrays.fill(Array128, (byte)0);
-			for(int i = 0; i < tempArray.length; i++) {
-				Array128[i] = tempArray[i];
-			}
-			Array128[tempArray.length] = -128;
-						
-			try {
-				MessageDigest.getInstance("SHA-512");
-			} catch (NoSuchAlgorithmException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
+		if(128 - mod > 8) {
+			blocks[amount - 1] = new byte[128];
+			Arrays.fill(blocks[amount - 1], (byte)0);
 			
+			for(int i = 0; i < 128 - mod; i++) {
+				blocks[amount - 1][i] = array[(amount - 1) * 128 + i];
+			}
+			blocks[amount - 1][128 - mod] = (byte)-128;
+			
+			byte[] temp = new BigInteger("" + array.length).multiply(BigInteger.valueOf(8)).toByteArray();
+			for(int i = temp.length - 1, j = 127; i >= 0; i--, j--) {
+				blocks[amount - 1][j] = temp[i];
+			}
 		} else {
+			blocks[amount - 2] = new byte[128];
+			Arrays.fill(blocks[amount - 2], (byte)0);
 			
+			for(int i = 0; i < 128 - mod; i++) {
+				blocks[amount - 2][i] = array[(amount - 2) * 128 + i];
+			}
+			blocks[amount - 2][128 - mod] = (byte)-128;
+			
+			
+			blocks[amount - 1] = new byte[128];
+			Arrays.fill(blocks[amount - 1], (byte)0);
+			
+			byte[] temp = new BigInteger("" + array.length).multiply(BigInteger.valueOf(8)).toByteArray();
+			for(int i = temp.length - 1, j = 127; i >= 0; i--, j--) {
+				blocks[(amount - 1) - 1][j] = temp[i];
+			}
 		}
+		
+		return blocks;
+	}
+	public static byte[][] get8BytesBlocks(byte[] array) {
+		byte[][] blocks = new byte[16][];		
+		for(int i = 0; i < blocks.length; ++i) {
+			blocks[i] = Arrays.copyOfRange(array, i * 8, (i + 1) * 8);
+		}
+		
+		return blocks;
 	}
 	
-	public void Hash(String text) {
-		BigInteger bitText = new BigInteger(text.getBytes());
+	// "../resource/StartProperties.txt"
+	public static JsonObject readStartProperties(String address) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new FileReader(address));
+		/*
+		CharBuffer buffer = CharBuffer.allocate(4096);
+		bufferedReader.read(buffer);
+		bufferedReader.close();
+		*/
+		JsonObject jObj = (JsonObject)new JsonParser().parse(bufferedReader);
+		bufferedReader.close();
+		
+		return jObj;
+	}
+	
+	public static byte[] rightCyclicShift(byte[] base, int n) {
+		char[] temp = new BigInteger(base).toString(2).toCharArray();
+		char[] full = new char[base.length * 8];
+		Arrays.fill(full, '0');
+		for(int i = full.length - 1; i >= full.length - temp.length; i--) {
+			full[i] = temp[i - (full.length - temp.length)];
+		}
+		
+		char[] part1 = Arrays.copyOfRange(full, 0, full.length - n);
+		char[] part2 = Arrays.copyOfRange(full, full.length - n, full.length);
+
+		for(int i = 0; i < part2.length; ++i) {
+			full[i] = part2[i];
+		}
+		
+		for(int i = 0; i < part1.length; ++i) {
+			full[n + i] = part1[i];
+		}
+		
+		return new BigInteger(String.valueOf(full), 2).toByteArray();
+	}
+	
+	public static byte[] rightShift(byte[] base, int n) {
+		char[] temp = new BigInteger(base).toString(2).toCharArray();
+		
+		for(int i = temp.length - 4; i >= 0; i--) {
+			temp[i + 3] = temp[i];
+			temp[i] = '0';
+		}
+	
+		return new BigInteger(String.valueOf(temp), 2).toByteArray();		
+	}
+	
+	
+	
+	
+	
+	
+	public static byte[] Maj(byte[] x, byte[] y, byte[] z) {
+		return Or(Or(And(x, y), And(x, z)), And(x, z));
+	}
+	
+	public static byte[] Ch(byte[] x, byte[] y, byte[] z) {
+		return Or(And(x, y), And(No(x), z));
+	}
+	
+	public static byte[] Eps_512_0(byte[] array) {
+		return Xor(Xor(rightCyclicShift(array, 28), rightCyclicShift(array, 34)), rightCyclicShift(array, 39));
+	}
+	
+	public static byte[] Eps_512_1(byte[] array) {
+		return Xor(Xor(rightCyclicShift(array, 14), rightCyclicShift(array, 18)), rightCyclicShift(array, 41));
+	}
+		
+	
+	public static byte[] Ro_512_0(byte[] array) {
+		return Xor(Xor(rightCyclicShift(array, 1), rightCyclicShift(array, 8)), rightShift(array, 7));
+	}
+	
+	public static byte[] Ro_512_1(byte[] array) {
+		return Xor(Xor(rightCyclicShift(array, 19), rightCyclicShift(array, 61)), rightShift(array, 6));
+	}
+	
+	public static byte[] Xor(byte[] a, byte[] b) {
+		int minSize = a.length > b.length ? b.length: a.length;
+		byte[] max = a.length > b.length ? a: b;
+		byte[] array = new byte[a.length > b.length ? a.length: b.length];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = max[i];
+		}
+		
+		for(int i = 0; i < minSize; i++) {
+			array[i] = (byte) (a[i] ^ b[i]);
+		}
+		
+		return array;
+	}
+	
+	public static byte[] Or(byte[] a, byte[] b) {
+		int minSize = a.length > b.length ? b.length: a.length;
+		byte[] max = a.length > b.length ? a: b;
+		byte[] array = new byte[a.length > b.length ? a.length: b.length];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = max[i];
+		}
+		
+		for(int i = 0; i < minSize; i++) {
+			array[i] = (byte) (a[i] | b[i]);
+		}
+		
+		return array;
+	}
+	
+	public static byte[] And(byte[] a, byte[] b) {
+		int minSize = a.length > b.length ? b.length: a.length;
+		byte[] max = a.length > b.length ? a: b;
+		byte[] array = new byte[a.length > b.length ? a.length: b.length];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = max[i];
+		}
+				
+		for(int i = 0; i < minSize; i++) {
+			array[i] = (byte) (a[i] & b[i]);
+		}
+		
+		return array;
+	}
+	
+	public static byte[] No(byte[] a) {
+		byte[] array = new byte[a.length];
+		
+		for(int i = 0; i < array.length; i++) {
+			array[i] =  (byte) ((byte) a[i] ^ (-128));
+		}
+		
+		return array;
+	}
+	
+	public void Hash(String text) {	
 		
 		
 	}
--- src/ru/start/Start.java
+++ src/ru/start/Start.java
@@ -1,21 +1,67 @@
 package ru.start;
 
+import java.io.IOException;
 import java.math.BigInteger;
 import java.util.Arrays;
+import java.lang.Integer;
+import ru.objects.SHA512;
 
 public class Start {
 
 	public static void main(String[] args) {
-		byte[] array = new byte[3];
-		Arrays.fill(array, (byte)1);
+		byte a = 1;
+		System.out.print(a ^ (-128));
 		
+		/*
+		byte[] array = new byte[1020];
+		for(int i = 0; i < array.length; i++) {
+			array[i] = (byte)(i % 128);
+		}
 		
-		BigInteger number = new BigInteger(array);
-		System.out.println(number);
-		System.out.println(number.bitCount());
-		System.out.println(number.bitLength());
-		System.out.println(number.toByteArray().length);
+		byte[][] cube = SHA512.get128BytesBlocks(array);
+		for(int i = 0; i < cube.length; i++) {
+			for(int j = 0; j < cube[i].length; j++) {
+				System.out.print(cube[i][j] + " ");				
+			}
+			System.out.println();	
+		}
 		
+		byte[][] cube2 = SHA512.get8BytesBlocks(cube[0]);
+		for(int i = 0; i < cube2.length; i++) {
+			for(int j = 0; j < cube2[i].length; j++) {
+				System.out.print(cube2[i][j] + " ");				
+			}
+			System.out.println();	
+		}
+		*/
+		/*
+		BigInteger a = new BigInteger("101000001000000001", 2);
+		System.out.println("a: " + a.toString(2));
+		
+		BigInteger b = new BigInteger(SHA512.No(a.toByteArray()));
+		System.out.println("b: " + b.toString(2));
+		*/
 	}
+	
+	public static byte[] f(byte[] base, int n) {
+		char[] temp = new BigInteger(base).toString(2).toCharArray();
+		char[] full = new char[base.length * 8];
+		Arrays.fill(full, '0');
+		for(int i = full.length - 1; i >= full.length - temp.length; i--) {
+			full[i] = temp[i - (full.length - temp.length)];
+		}
+		
+		char[] part1 = Arrays.copyOfRange(full, 0, full.length - n);
+		char[] part2 = Arrays.copyOfRange(full, full.length - n, full.length);
 
+		for(int i = 0; i < part2.length; ++i) {
+			full[i] = part2[i];
+		}
+		
+		for(int i = 0; i < part1.length; ++i) {
+			full[n + i] = part1[i];
+		}
+	
+		return new BigInteger(String.valueOf(full), 2).toByteArray();
+	}
 }
